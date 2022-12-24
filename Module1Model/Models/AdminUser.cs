d8d73de9015816ac7a0ac1b9c3752a0a6b383e28
mateoqwe <mateoqwe12@gmail.com>
2022-12-24 19:28:20 +0700

    Initial commit

---
+++ Module1Model/Models/AdminUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1Model.Models
+{
+    public class AdminUser
+    {
+        [Key]
+        public int adminUserId { get; set; }
+
+        [Required]
+        public string userName { get; set; }
+
+        [Required]
+        public string password { get; set; }
+    }
+}
---
+++ Module1Model/Models/AdminUserRole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1Model.Models
+{
+    public class AdminUserRole
+    {
+        [Key]
+        public int adminUserRoleId { get; set; }
+
+        [Required]
+        public string roleName { get; set; }
+    }
+}
